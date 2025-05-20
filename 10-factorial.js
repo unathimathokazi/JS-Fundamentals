75335fb592cea69080efe215496a973a9e69e1c3
Unathi Mathokazi <unathimathokaziu@gmail.com>
2025-05-20 13:51:16 +0200

    Add script to compute factorial recursively

---
+++ 10-factorial.js
@@ -0,0 +1,13 @@
+const num = parseInt(process.argv[2]);
+
+function factorial(n) {
+  if (isNaN(n) || n < 0) {
+    return 1;
+  }
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(num));
